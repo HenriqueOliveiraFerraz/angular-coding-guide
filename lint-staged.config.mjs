4cf404d8455acd3217328064f01fcf38c6eaca0f
HenriqueOliveiraFerraz <henrique.oliveira.ferraz@protonmail.com>
2025-04-06 23:04:36 -0300

    chore: adapted lint staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -3,7 +3,7 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  'projects/task-management-app/**': ['npm run eslint:task-management-app:fix'],
-  'projects/task-management-api/**': ['npm run eslint:task-management-api:fix'],
+  'projects/task-management-app/**/*.{js,ts,html}': ['npm run eslint:task-management-app:fix'],
+  'projects/task-management-api/**/*.{js,ts,html}': ['npm run eslint:task-management-api:fix'],
   '*': ['npm run prettier:write'],
 };
