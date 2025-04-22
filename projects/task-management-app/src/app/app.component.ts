11a62e9fc208c50a5c831c96176ccd521d103370
HenriqueOliveiraFerraz <henrique.oliveira.ferraz@protonmail.com>
2025-04-22 09:53:33 -0300

    chore: nx init command executed to migration to nx

--- projects/task-management-app/src/app/app.component.ts
+++ projects/task-management-app/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskManagementApiService } from '@task-management-api';
+import { SharedModule } from '@task-management-app/shared/modules/shared.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'task-management-app';
   taskManagementApiService = inject(TaskManagementApiService);
+  tasksObs = this.taskManagementApiService.getTasks();
 }
